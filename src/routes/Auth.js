2f74ef91366902d447a3a5284692c7d3058b71cb
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-16 02:23:17 +0700

    update authentication end point for register etc

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,20 +1,13 @@
 const Auth = require('express').Router()
-const multer = require('multer')
 const AuthController = require('../controllers/Auth')
 const AuthToken = require('../middleware/Auth')
-const storage = multer.diskStorage({
-  destination: 'files/',
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now}-${file.originalname}`)
-  }
-})
 
-const upload = multer({ storage })
-Auth.post('/picture', upload.single('picture'), AuthController.register)
 Auth.post('/register', AuthController.register)
-Auth.get('/activate', AuthController.verify)
-Auth.post('/login', AuthController.login)
-Auth.post('/forgot-password', AuthController.forgetPass)
+Auth.get('/verify', AuthController.verify)
+Auth.patch('/security/:id', AuthController.madeSecurity)
+Auth.post('/security/check/:id', AuthController.securityCheck)
+Auth.post('/signin', AuthController.signIn)
+Auth.post('/security/forgot/:id', AuthController.forgotSecurity)
 Auth.get('/user', AuthToken.checkToken, function (req, res) {
   res.send(req.user)
 })
