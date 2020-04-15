45ab252271d5b720fc471844336e3ec548696312
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-16 02:26:08 +0700

    update auth flow

--- App.js
+++ App.js
@@ -10,11 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false })) // for x-www-urlencoded
 app.use(bodyParser.json()) // for json
 app.use(cors('*'))
 
-// const AuthRouter = require('./src/routes/Auth')
+const AuthRouter = require('./src/routes/Auth')
 const MigrateRouter = require('./src/routes/migrations')
 
 app.use('/migrate', MigrateRouter)
-// app.use('/auth', AuthRouter) // first register here
+app.use('/auth', AuthRouter) // first register here
 
 app.listen(process.env.PORT, () => {
   console.log(`APP listen on ${process.env.PORT}`)
