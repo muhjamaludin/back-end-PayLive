86dc21ebb9e9c21bc92ecf91e235ac6b5c26e847
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-16 02:24:35 +0700

    add authentication controller fot register etc

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -9,52 +9,73 @@ const uuid = require('uuid').v4
 
 module.exports = {
   register: async function (req, res) {
-    console.log('a')
-    const { username, password, name, email, phone } = req.body
-    // const picture = (req.file && req.file.filename) || null
-    const checkUser = await AuthModel.checkUsername(username)
-    if (checkUser !== 0) {
-      const data = {
-        success: false,
-        msg: 'Username already used'
-      }
-      res.send(data)
-    } else {
-      const encrypPass = bcrypt.hashSync(password)
-      const results = await UserModel.createUser(username, encrypPass)
-      const info = await AuthModel.getUserByUsername(username)
-      await UserdModel.createUserDetail(info.id, name, email, phone, 0)
-      if (results) {
-        if (await AuthModel.createVerificationCode(results, uuid())) {
-          const code = await AuthModel.getVerificationCode(username)
-          const data = {
-            success: true,
-            msg: 'register success',
-            code
+    console.log('register guys')
+    const { fullname, phone, email } = req.body
+    const checkUser = await AuthModel.checkPhone(phone)
+    const valid = phone.length > 10 && phone.length < 13
+    if (valid) {
+      if (checkUser !== 0) {
+        const data = {
+          success: false,
+          msg: 'Phone already registered'
+        }
+        res.send(data)
+      } else {
+        const results = await UserModel.createUser(phone)
+        const info = await AuthModel.getUserByPhone(phone)
+        await UserdModel.createUserDetail(info.id, fullname, email)
+        if (results) {
+          if (await AuthModel.createVerificationCode(results, uuid())) {
+            const code = await AuthModel.getVerificationCode(phone)
+            const data = {
+              success: true,
+              msg: `register for phone number ${phone} success \n waiting for verify`,
+              code
+            }
+            res.send(data)
+          } else {
+            const data = {
+              success: false,
+              msg: 'Verification code couldn\'t be generate'
+            }
+            res.send(data)
           }
-          res.send(data)
         } else {
           const data = {
             success: false,
-            msg: 'Verification code couldn\'t be generate'
+            msg: 'register failed'
           }
           res.send(data)
         }
-      } else {
-        const data = {
-          success: false,
-          msg: 'register failed'
-        }
-        res.send(data)
       }
+    } else {
+      const data = {
+        succes: false,
+        msg: 'Phone number is not valid'
+      }
+      res.send(data)
+    }
+  },
+  madeSecurity: async function (req, res) {
+    const { securityCode } = req.body
+    const id = req.params.id
+    const encrypPass = bcrypt.hashSync(securityCode)
+    console.log(id, securityCode)
+    const results = await UserModel.createSecurityCode(id, encrypPass)
+    if (results) {
+      const data = {
+        succes: true,
+        msg: 'security_code has been created, You can go home now'
+      }
+      res.send(data)
     }
   },
   verify: async function (req, res) {
-    const { username, code } = req.query
-    if (await AuthModel.verifyUser(username, code)) {
+    const { phone, code } = req.query
+    if (await AuthModel.verifyUser(phone, code)) {
       const data = {
         success: true,
-        msg: 'User activated succesfully'
+        msg: 'User activated succesfully Please made security code'
       }
       res.send(data)
     } else {
@@ -65,51 +86,62 @@ module.exports = {
       res.send(data)
     }
   },
-  login: async function (req, res) {
-    const { username, password } = req.body
-    const checkUser = await AuthModel.checkUsername(username)
-    if (!checkUser) {
-      const data = {
-        success: false,
-        msg: 'Wrong username'
-      }
-      res.send(data)
-    } else {
-      const info = await AuthModel.getUserByUsername(username)
-      const checkPass = bcrypt.compareSync(password, info.password)
-      if (checkPass) {
-        if (await AuthModel.checkVerifiedUser) {
-          if (await AuthModel.checkActivatedUser) {
-            const payload = { id: info.id, username, roleId: info.role_id }
-            const options = { expiresIn: '600m' }
-            const key = process.env.APP_KEY
-            const token = jwt.sign(payload, key, options)
-            const data = {
-              success: true,
-              token
-            }
-            res.send(data)
-          } else {
-            const data = {
-              success: false,
-              msg: 'Username not active'
-            }
-            res.send(data)
+  securityCheck: async function (req, res) {
+    const { id } = req.params
+    const { securityCode } = req.body
+    console.log(id, securityCode)
+    const info = await AuthModel.getUserById(id)
+    const checkPass = await bcrypt.compareSync(securityCode, info.security_code)
+    if (checkPass) {
+      if (await AuthModel.checkVerifiedUser) {
+        if (await AuthModel.checkActivatedUser) {
+          const payload = { id: info.id, securityCode, roleId: info.role_id }
+          const options = { expiresIn: '30m' }
+          const key = process.env.APP_KEY
+          const token = jwt.sign(payload, key, options)
+          const data = {
+            success: true,
+            token,
+            msg: 'Let\'s go Home with token'
           }
+          res.send(data)
         } else {
           const data = {
             success: false,
-            msg: 'Username not verified'
+            msg: 'Phone not active'
           }
           res.send(data)
         }
       } else {
         const data = {
           success: false,
-          msg: 'Wrong Password'
+          msg: 'Phone not verified'
         }
         res.send(data)
       }
+    } else {
+      const data = {
+        success: false,
+        msg: 'Wrong Security Code'
+      }
+      res.send(data)
+    }
+  },
+  signIn: async function (req, res) {
+    const { phone } = req.body
+    const checkUser = await AuthModel.checkPhone(phone)
+    if (!checkUser) {
+      const data = {
+        success: false,
+        msg: 'Your phone number is not registered'
+      }
+      res.send(data)
+    } else {
+      const data = {
+        succes: true,
+        msg: 'Phone available, Please Verify and insert your security code to enjoy the feature'
+      }
+      res.send(data)
     }
   },
   forgetPass: async function (req, res) {
