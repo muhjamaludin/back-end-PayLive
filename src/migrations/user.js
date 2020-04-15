a27306b1de9788a389d3825334c4cd3c8714c341
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-15 15:19:57 +0700

    change password to security_code

--- src/migrations/user.js
+++ src/migrations/user.js
@@ -12,7 +12,7 @@ db.query(
   `CREATE TABLE IF NOT EXISTS users(
   id INT PRIMARY KEY AUTO_INCREMENT,
   phone INT(14) NOT NULL,
-  password VARCHAR(60) NOT NULL,
+  security_code VARCHAR(60) NOT NULL,
   verification_code VARCHAR(37),
   is_active TINYINT(2) DEFAULT 0,
   is_verified TINYINT(2) DEFAULT 0,
