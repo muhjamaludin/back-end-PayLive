b663e4d58e6cc1e8231f5846987d21c6a3f35c3f
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-15 14:31:08 +0700

    add migrations database

--- src/migrations/user.js
+++ src/migrations/user.js
@@ -1,14 +1,36 @@
 const db = require('../utils/db')
 
-db.query(`CREATE TABLE IF NOT EXISTS users(
+const check = function (err, results, fields) {
+  if (err) {
+    throw err
+  }
+  console.log(results)
+  console.log(fields)
+}
+
+db.query(
+  `CREATE TABLE IF NOT EXISTS users(
   id INT PRIMARY KEY AUTO_INCREMENT,
-  picture TEXT,
-  username VARCHAR(60) NOT NULL,
+  phone INT(14) NOT NULL,
   password VARCHAR(60) NOT NULL,
   verification_code VARCHAR(37),
   is_active TINYINT(2) DEFAULT 0,
   is_verified TINYINT(2) DEFAULT 0,
+  role_id TINYINT(2) DEFAULT 2,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
-  UNIQUE  KEY unique_username (username),
-  )`, function () {})
+  updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
+  )
+`, check)
+
+db.query(`
+  CREATE TABLE IF NOT EXISTS user_details(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(30) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    profil_picture TEXT,
+    identity_picture TEXT,
+    balance INT(8) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
+  )
+`, check)
