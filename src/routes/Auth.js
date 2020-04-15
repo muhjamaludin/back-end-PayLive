0b61458202ffe6ed19b2c697cd9aed88de8050e8
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-16 03:04:42 +0700

    add end point edit security code

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -7,7 +7,7 @@ Auth.get('/verify', AuthController.verify)
 Auth.patch('/security/:id', AuthController.madeSecurity)
 Auth.post('/security/check/:id', AuthController.securityCheck)
 Auth.post('/signin', AuthController.signIn)
-Auth.post('/security/forgot/:id', AuthController.forgotSecurity)
+Auth.post('/security/edit/:id', AuthController.forgotSecurity)
 Auth.get('/user', AuthToken.checkToken, function (req, res) {
   res.send(req.user)
 })
