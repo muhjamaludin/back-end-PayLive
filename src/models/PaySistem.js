dd2614950b16c340b6cd6ab71b64933739c75b5e
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-16 16:28:38 +0700

    file to made filesystem

---
+++ src/models/PaySistem.js
@@ -0,0 +1,87 @@
+const db = require('../utils/db')
+module.exports = {
+  getAllCategory: function () {
+    const table = 'category'
+    return new Promise(function (resolve, reject) {
+      const query = `SELECT * FROM ${table}`
+      db.query(query, function (err, results, fields) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(results)
+        }
+      })
+    })
+  },
+  getCategoryById: function (id) {
+    const table = 'category'
+    const query = `SELECT * FROM ${table} WHERE id=${id}`
+    return new Promise(function (resolve, reject) {
+      db.query(query, function (err, results, fields) {
+        if (err) {
+          reject(err)
+        } else {
+          if (results.length) {
+            resolve(results[0])
+          } else {
+            resolve(false)
+          }
+        }
+      })
+    })
+  },
+  createPaySistem: function (idMenu, name) {
+    const table = 'pay_sistems'
+    return new Promise(function (resolve, reject) {
+      const query = `INSERT INTO ${table} (id_menu, name) VALUES (${idMenu}, '${name}')`
+      console.log(query)
+      db.query(query, function (err, results, fields) {
+        if (err) {
+          reject(err)
+        } else {
+          if (results.affectedRows) {
+            resolve(results.affectedRows)
+          } else {
+            resolve(false)
+          }
+        }
+      })
+    })
+  },
+  updateCategory: function (id, name) {
+    const table = 'category'
+    return new Promise(function (resolve, reject) {
+      const query = `UPDATE ${table} SET name='${name}' WHERE id=${id}`
+      console.log(query)
+      db.query(query, function (err, results, fields) {
+        if (err) {
+          reject(err)
+        } else {
+          if (results.affectedRows) {
+            resolve(results.affectedRows)
+          } else {
+            resolve(false)
+          }
+        }
+      })
+    })
+  },
+  deleteCategory: function (id) {
+    const table = 'category'
+    return new Promise(function (resolve, reject) {
+      const query = ` DELETE FROM ${table} WHERE id= ${id}`
+      console.log(query)
+      db.query(query, function (err, results, fields) {
+        if (err) {
+          reject(err)
+        } else {
+          if (results.affectedRows) {
+            resolve(true)
+          } else {
+            resolve(false)
+          }
+        }
+      })
+    })
+  }
+}
