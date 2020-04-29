ba2221f693fc37150d5f23b4d013ed5184368694
ainna2020 <ainayaass@gmail.com>
2020-04-29 09:22:59 +0700

    pagination in history

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -18,8 +18,12 @@ module.exports = {
     const results = await UserModel.getAllUsers(conditions)
     conditions.totalData = await UserModel.getTotalUsers(conditions)
     conditions.totalPage = Math.ceil(conditions.totalData / conditions.perPage)
-    conditions.nextLink = (page >= conditions.totalPage ? null : process.env.APP_URI.concat(`users?page=${page + 1}`))
-    conditions.prevLink = (page <= 1 ? null : process.env.APP_URI.concat(`users?page=${page - 1}`))
+    conditions.nextLink =
+      page >= conditions.totalPage
+        ? null
+        : process.env.APP_URI.concat(`users?page=${page + 1}`)
+    conditions.prevLink =
+      page <= 1 ? null : process.env.APP_URI.concat(`users?page=${page - 1}`)
     delete conditions.search
     delete conditions.sort
     delete conditions.limit
@@ -27,21 +31,21 @@ module.exports = {
     const data = {
       success: true,
       data: results,
-      pageInfo: conditions
+      pageInfo: conditions,
     }
     res.send(data)
   },
   getUser: async function (req, res) {
     const data = {
       success: true,
-      data: await UserModel.getUserById(req.params.id)
+      data: await UserModel.getUserById(req.params.id),
     }
     res.send(data)
   },
   getDetails: async function (req, res) {
     const data = {
       success: true,
-      data: await UserModel.getUserById(req.params.id)
+      data: await UserModel.getUserById(req.params.id),
     }
     res.send(data)
   },
@@ -59,13 +63,13 @@ module.exports = {
         const data = {
           success: true,
           msg: `User with phone ${phone} has been updated!`,
-          data: { id, ...req.body }
+          data: { id, ...req.body },
         }
         res.send(data)
       } else {
         const data = {
           success: false,
-          msg: 'There is no data can be updated'
+          msg: 'There is no data can be updated',
         }
         res.send(data)
       }
@@ -81,13 +85,13 @@ module.exports = {
       const data = {
         success: true,
         msg: `Photo for id ${idUser} has been updated!`,
-        data: { idUser, ...req.file }
+        data: { idUser, ...req.file },
       }
       res.send(data)
     } else {
       const data = {
         success: false,
-        msg: 'There is no data can be updated'
+        msg: 'There is no data can be updated',
       }
       res.send(data)
     }
@@ -100,13 +104,13 @@ module.exports = {
     if (results) {
       const data = {
         success: true,
-        msg: `Users with id ${id} has been deleted!`
+        msg: `Users with id ${id} has been deleted!`,
       }
       res.send(data)
     } else {
       const data = {
         success: false,
-        msg: 'There is no data can be deleted'
+        msg: 'There is no data can be deleted',
       }
       res.send(data)
     }
@@ -118,21 +122,21 @@ module.exports = {
     if (topup < 0) {
       const data = {
         success: false,
-        msg: 'Wrong input balance'
+        msg: 'Wrong input balance',
       }
       res.send(data)
     } else {
       if (topup > 0 && topup < 10000) {
         const data = {
           success: false,
-          msg: 'Minimum top up is Rp 10.000,00'
+          msg: 'Minimum top up is Rp 10.000,00',
         }
         res.send(data)
       } else {
         if (topup > 1000000) {
           const data = {
             success: false,
-            msg: 'Maximum top up is Rp 1.000.000,00 Rupiah'
+            msg: 'Maximum top up is Rp 1.000.000,00 Rupiah',
           }
           res.send(data)
         } else {
@@ -143,13 +147,13 @@ module.exports = {
             const data = {
               success: true,
               msg: `Your cash has been added with ${balance} rupiah`,
-              data: { ...result }
+              data: { ...result },
             }
             res.send(data)
           } else {
             const data = {
               success: false,
-              msg: 'failed topup, your account not found'
+              msg: 'failed topup, your account not found',
             }
             res.send(data)
           }
@@ -163,13 +167,13 @@ module.exports = {
     if (results) {
       const data = {
         success: true,
-        data: { idUser, ...results }
+        data: { idUser, ...results },
       }
       res.send(data)
     } else {
       const data = {
         success: false,
-        msg: 'There is no data for your request'
+        msg: 'There is no data for your request',
       }
       res.send(data)
     }
@@ -189,13 +193,13 @@ module.exports = {
         const data = {
           success: true,
           msg: `Your amount transfer Rp ${amount},00 Rupiah has been sent`,
-          data: { idUser, ...results }
+          data: { idUser, ...results },
         }
         res.send(data)
       } else {
         const data = {
           success: false,
-          msg: 'Your access has been wrong input'
+          msg: 'Your access has been wrong input',
         }
         res.send(data)
       }
@@ -204,30 +208,65 @@ module.exports = {
     }
   },
   getHistory: async function (req, res) {
+    let { page, limit, search, sort } = req.query
+    page = parseInt(page) || 1
+    limit = parseInt(limit) || 5
+
+    let key = search && Object.keys(search)[0]
+    let value = search && Object.values(search)[0]
+    search = (search && { key, value }) || { key: 'phone', value: '' }
+
+    key = sort && Object.keys(sort)[0]
+    value = sort && Object.values(sort)[0]
+    sort = (sort && { key, value }) || { key: 'id', value: 1 }
+    const conditions = { page, perPage: limit, search, sort }
     const { idUser } = req.params
-    const count = await UserModel.getAllHistory(idUser)
-    if (count.total === 0) {
-      const data = {
-        success: false,
-        msg: 'No data'
-      }
-      res.send(data)
-    } else {
-      const results = await UserModel.getHistory(idUser)
-      if (results) {
-        const data = {
-          success: true,
-          idUser: idUser,
-          data: results
-        }
-        res.send(data)
-      } else {
-        const data = {
-          success: false,
-          msg: 'There is no data for your request'
-        }
-        res.send(data)
-      }
+    const results = await UserModel.getHistory(conditions, idUser)
+    const totalData = await UserModel.getAllHistory(idUser)
+    console.log('total data', totalData)
+    conditions.totalData = totalData.total
+    conditions.totalPage = Math.ceil(conditions.totalData / conditions.perPage)
+    conditions.nextLink =
+      page >= conditions.totalPage
+        ? null
+        : process.env.APP_URI.concat(`users?page=${page + 1}`)
+    conditions.prevLink =
+      page <= 1 ? null : process.env.APP_URI.concat(`users?page=${page - 1}`)
+    delete conditions.search
+    delete conditions.sort
+    delete conditions.limit
+
+    const data = {
+      success: true,
+      pageInfo: conditions,
+      data: results,
     }
+    res.send(data)
   },
+
+  //   const count = await UserModel.getAllHistory(idUser)
+  //   if (count.total === 0) {
+  //     const data = {
+  //       success: false,
+  //       msg: 'No data'
+  //     }
+  //     res.send(data)
+  //   } else {
+  //     const results = await UserModel.getHistory(idUser)
+  //     if (results) {
+  //       const data = {
+  //         success: true,
+  //         idUser: idUser,
+  //         data: results
+  //       }
+  //       res.send(data)
+  //     } else {
+  //       const data = {
+  //         success: false,
+  //         msg: 'There is no data for your request'
+  //       }
+  //       res.send(data)
+  //     }
+  //   }
+  // },
 }
